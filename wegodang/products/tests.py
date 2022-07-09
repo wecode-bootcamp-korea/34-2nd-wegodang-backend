88b3dfff7b45443fd6c92c5affc869935cfc3091
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-09 14:37:16 +0900

    CREATE: CREATE_CATEGORY_VIEW

--- wegodang/products/tests.py
+++ wegodang/products/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from products.models import Category
 
-# Create your tests here.
+class CategoryViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        Category.objects.create(id=1, name="테스트", image="테스트.url")
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_success_get_categories(self):
+        response = self.client.get('/products/categories')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.json(),
+        {
+            "result": {
+                "categories": [
+                    {
+                        "id"    : 1,
+                        "name"  : "테스트",
+                        "image" : "테스트.url"
+                    }
+                ]
+            }
+        }
+        )
