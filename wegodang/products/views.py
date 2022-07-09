29076852634993e2728f092474d3c33f7402ae24
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-09 14:57:52 +0900

    CREATE: CREATE_PRODUCT_LIST_VIEW

--- wegodang/products/views.py
+++ wegodang/products/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import jwt
+import json
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
+
+from products.models  import Category, Product, ProductModel, ProductImage
+
+class ProductListView(View):
+    def get(self, request):
+        category = request.GET.get('category_id')
+        offset   = int(request.GET.get('offset', 0))
+        limit    = int(request.GET.get('limit', 5))
+
+        q = Q()
+
+        if category:
+            q &= Q(category_id=category)
+
+        products = Product.objects.filter(q)[offset:offset+limit]
+
+        products = [{
+            "product_id"     : product.id,
+            "product_name"   : product.name,
+            "category_name"  : product.category.name,
+            "user"           : product.user.user_name,
+            "goal_percent"   : int(product.total_price / product.goal_price * 100),
+            "goal_price"     : int(product.goal_price),
+            "remaining_days" : (product.end_date - product.start_date.today()).days,
+            "image_url"      : product.product_images.first().image_url
+            } for product in products ]
+
+        return JsonResponse({"products": products}, status=200)
--- wegodang/users/models.py
+++ wegodang/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    username     = models.CharField(max_length=45, unique=True)
+    user_name     = models.CharField(max_length=45, unique=True)
     email        = models.EmailField(max_length=200, unique=True)
     kakao_id     = models.BigIntegerField()
     created_at   = models.DateField(auto_now_add=True)
