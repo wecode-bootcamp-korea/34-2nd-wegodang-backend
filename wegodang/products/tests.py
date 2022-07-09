3c5529517307e9e4497016ea06a0902df8f13fd2
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-09 16:06:33 +0900

    CREATE: CREATE_PRODUCT_VIEW_CONFLICT_OK

--- wegodang/products/tests.py
+++ wegodang/products/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase, Client
-from products.models import Category
+from django.test     import TestCase, Client
+from products.models import Category, Product, ProductImage
+from users.models    import User
 
 class CategoryViewTest(TestCase):
     def setUp(self):
@@ -26,4 +27,71 @@ class CategoryViewTest(TestCase):
                 ]
             }
         }
+    )
+
+class ProductViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        Category.objects.create(
+            id   = 1,
+            name = "카테고리"
+        )
+
+        User.objects.create(
+            id         = 1,
+            kakao_id   = 1,
+            updated_at = "2022-01-02"
+        )
+
+        Product.objects.create(
+            id             = 1,
+            name           = "테스트",
+            total_price    = 10,
+            goal_price     = 10,
+            suppoters      = 10,
+            start_date     = "2022-01-01",
+            end_date       = "2022-07-12",
+            story          = "스토리",
+            slide_title    = "슬라이더_타이틀",
+            slide_subtitle = "슬라이더_서브타이틀",
+            category       = Category.objects.get(id=1),
+            user           = User.objects.get(id=1)
+            )
+
+        ProductImage.objects.create(
+            id        = 1,
+            product   = Product.objects.get(id=1),
+            image_url = "테스트.url"
+        )
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        User.objects.all().delete()
+        Product.objects.all().delete()
+        ProductImage.objects.all().delete()
+
+    def test_success_get_product_details(self):
+        response = self.client.get('/products/1')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.json(),
+            {
+                "products": {
+                    "product_id"     : 1,
+                    "product_name"   : "테스트",
+                    "goal_percent"   : 100,
+                    "total_price"    : 10,
+                    "suppoters"      : 10,
+                    "remaining_days" : 1,
+                    "story"          : "스토리",
+                    "image_url": [
+                        {
+                            "id"  : 1,
+                            "url" : "테스트.url"
+                        }
+                    ]
+                }
+            }
         )
--- wegodang/products/views.py
+++ wegodang/products/views.py
@@ -3,8 +3,9 @@ import json
 
 from django.http      import JsonResponse
 from django.views     import View
+from datetime         import date
 
-from products.models  import Category
+from products.models  import Category, Product
 
 class CategoryView(View):
     def get(self, request):
@@ -14,3 +15,29 @@ class CategoryView(View):
         }
 
         return JsonResponse({"result" : result}, status=200)
+
+class ProductView(View):
+    def get(self, request, product_id):
+        product = Product.objects.get(id = product_id)
+        now     = date.today()
+
+        product_images = product.product_images.all()
+
+        product_images = [{
+            "id"  : product_image.id,
+            "url" : product_image.image_url
+        } for product_image in product_images ]
+
+        products = {
+            "product_id"     : product.id,
+            "product_name"   : product.name,
+            "goal_percent"   : int(product.total_price / product.goal_price * 100),
+            "total_price"    : int(product.total_price),
+            "suppoters"      : product.suppoters,
+            "remaining_days" : (product.end_date - now).days,
+            "story"          : product.story,
+            "image_url"      : product_images
+        }
+        
+        return JsonResponse({"products": products}, status=200)
+        
