cc69a262860de582e9c81539df92f038ffaa17cb
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-09 15:28:40 +0900

    CREATE: CREATE_SLIDER_VIEW

--- wegodang/products/views.py
+++ wegodang/products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import jwt
+import json
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
+
+from products.models  import Category, Product, ProductModel, ProductImage
+
+class SliderView(View):
+    def get(self, request):
+
+        products = Product.objects.order_by('?')[0:8]
+
+        products = [{
+            "image_url"      : product.product_images.first().image_url,
+            "slide_title"    : product.slide_title,
+            "slide_subtitle" : product.slide_subtitle
+        } for product in products]
+
+        return JsonResponse({"products": products}, status=200)
