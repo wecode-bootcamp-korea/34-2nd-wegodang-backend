a6ad2e9997f715401e4e9d2abe01b1b093cced45
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-09 16:06:33 +0900

    CREATE: CREATE_PRODUCT_VIEW_CONFLICT_OK

--- wegodang/products/urls.py
+++ wegodang/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from products.views import CategoryView
+from products.views import CategoryView, ProductView
 
 urlpatterns = [
-    path('/categories', CategoryView.as_view())
+    path('/categories', CategoryView.as_view()),
+    path('/<int:product_id>', ProductView.as_view())
 ]
