f164b84ff0a4821feacf7456ca6d09b4fc250e90
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-10 13:44:23 +0900

    CREATE: CREATE_PRODUCT_LIST_SLIDER_VIEW_WITH_RESOLVED_CONFLICT

--- wegodang/products/urls.py
+++ wegodang/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from products.views import CategoryView, ProductView
+from products.views import CategoryView, ProductView, ProductListView
 
 urlpatterns = [
     path('/categories', CategoryView.as_view()),
-    path('/<int:product_id>', ProductView.as_view())
+    path('/<int:product_id>', ProductView.as_view()),
+    path('', ProductListView.as_view())
 ]
