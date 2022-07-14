5cd97b7cd125a324d26d49930852329ebcf62cfb
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-14 18:09:57 +0900

    CREATE: CREATE_PROFILE_UPLOAD

--- wegodang/users/urls.py
+++ wegodang/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import KaKaoSignUpView
+from users.views import KaKaoSignUpView, ProfileView
 
 urlpatterns = [
-    path('/kakao', KaKaoSignUpView.as_view())
+    path('/kakao', KaKaoSignUpView.as_view()),
+    path('/profile', ProfileView.as_view())
 ]
