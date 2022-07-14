7cec4cb4bd2163ec8cd0223889f904034f15789c
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-07-14 18:09:57 +0900

    CREATE: CREATE_PROFILE_UPLOAD

--- wegodang/users/models.py
+++ wegodang/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    user_name    = models.CharField(max_length=45, unique=True)
-    email        = models.EmailField(max_length=200, unique=True)
-    kakao_id     = models.BigIntegerField()
-    created_at   = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now=True)
+    user_name     = models.CharField(max_length=45, unique=True)
+    email         = models.EmailField(max_length=200, unique=True)
+    kakao_id      = models.BigIntegerField()
+    profile_image = models.CharField(max_length=200, default="https://cdn.pixabay.com/photo/2017/09/10/18/25/question-2736480_1280.jpg")
+    created_at    = models.DateTimeField(auto_now_add=True)
+    updated_at    = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
